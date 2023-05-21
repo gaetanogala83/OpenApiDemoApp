d8eda1b1469d1cd817751619c86395e2eb6c8064
unknown <gaetanogala83@gmail.com>
2023-05-21 22:47:23 +0200

    first commit

---
+++ src/main/java/com/gala/gaetano/openapidemo/app/OpenApidemoApplication.java
@@ -0,0 +1,13 @@
+package com.gala.gaetano.openapidemo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenApidemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenApidemoApplication.class, args);
+    }
+
+}
